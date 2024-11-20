91cf9baa3e1eb168959043512c5f1e05c168d216
Raymond344 <153834090+Raymond344@users.noreply.github.com>
2024-11-20 19:27:31 +0700

    Add files via upload

---
+++ extras/cloudbuild/initFirebase.js
@@ -0,0 +1,27 @@
+// Copyright 2018 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Script for configuring Firebase.
+// See https://firebase.google.com/docs/web/setup for more information.
+
+var config = {
+    apiKey: "AIzaSyCaHeiX02OXKg5Yjs-S7NYY5yZaAtq2hlc",
+    authDomain: "serverless-61363.firebaseapp.com",
+    projectId: "serverless-61363",
+    storageBucket: "serverless-61363.firebasestorage.app",
+    messagingSenderId: "390680314871",
+    appId: "1:390680314871:web:3a776e9657c9ef173f64f2"
+};
+
+firebase.initializeApp(config);
